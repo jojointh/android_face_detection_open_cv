f9b0196272c984dc9c7f79eaf16e52c0f6eec743
jojointh <jojo141a@hotmail.com>
2017-11-22 17:21:59 +0700

    implement face detection

---
+++ app/src/main/java/com/nongmah/facedetectionopencv/OpencvClass.java
@@ -0,0 +1,9 @@
+package com.nongmah.facedetectionopencv;
+
+/**
+ * Created by ndtat on 2017-11-22.
+ */
+
+public class OpencvClass {
+    public native static void faceDetection(long addrRgba);
+}

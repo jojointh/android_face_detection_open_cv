b01a034db77f09dfa6fb9e47f2e5c0a0b942de53
jojointh <jojo141a@hotmail.com>
2017-11-22 19:53:38 +0700

    change face detection method using lbpcascade_frontalface following official open cv example

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -9,10 +9,10 @@ OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
 include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
 
-LOCAL_SRC_FILES := com_nongmah_facedetectionopencv_OpencvClass.cpp
+LOCAL_SRC_FILES := DetectionBasedTracker_jni.cpp
 
 LOCAL_LDLIBS += -llog
-LOCAL_MODULE := MyLibs
+LOCAL_MODULE := detection_based_tracker
 
 
 include $(BUILD_SHARED_LIBRARY)
